38aa49d11a83229a55a67a047d3babc3a8593e4f
Carter <cjhhankin@knights.ucf.edu>
2021-08-06 18:13:48 -0400

    Initial Author Routes

--- server.js
+++ server.js
@@ -6,8 +6,11 @@ if(process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
+// Creating the router
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 //Setting the view engine
 app.set('view engine', 'ejs')
@@ -15,13 +18,17 @@ app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
+// Due to deprecation added useUnifiedTopology: true,
+mongoose.connect(process.env.DATABASE_URL, {useUnifiedTopology: true, useNewUrlParser: true})
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
+// Using the router
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT  || 3000)
